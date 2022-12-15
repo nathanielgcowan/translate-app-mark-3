175e7c5a03ce81bc0bfb8e09ec0112c986cb348f
Nathaniel Cowan <nathaniel.g.cowan@gmail.com>
2022-12-15 14:21:24 -0600

    deploy with git two

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create fdafsfsdfsd App</title>
+        <title>Create fffffff App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
