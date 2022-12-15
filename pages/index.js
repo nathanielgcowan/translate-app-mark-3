a9b9727624c13ba6e119835e18f3b667362b603a
Nathaniel Cowan <nathaniel.g.cowan@gmail.com>
2022-12-15 14:48:43 -0600

    BBBB

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
+          AAAAA
         </p>
 
         <div className={styles.grid}>
